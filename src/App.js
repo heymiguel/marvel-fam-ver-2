ab1e827975573a478a3776b649650012d2038e39
heymiguel <miguelcesarbautista@gmail.com>
2017-01-26 22:01:01 -0500

    moment js!

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import Header from './Header.js';
 import DateSelector from './DateSelector.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDateSelect = this.handleDateSelect.bind(this);
+    this.state = {
+      date: ""
+    };
+  }
+
+  handleDateSelect(newDate) {
+    console.log(newDate);
+    this.setState({
+      date: newDate
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <DateSelector> </DateSelector>
+        <DateSelector selectDate={this.handleDateSelect}> </DateSelector>
+        <p>
+          {this.state.date}
+        </p>
       </div>
     )
   }
--- src/DateSelector.js
+++ src/DateSelector.js
@@ -7,21 +7,36 @@ class DateSelector extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.onDateSelect = this.onDateSelect.bind(this);
         this.state = {
-            startDate: moment()
+            startDate: moment(),
+            // have to start with a moment!!
+            // then grab what you need
+            selectedDate: ""
         };
     }
 
     handleChange(date) {
+        // let convertedDate = date.format();
+        // console.log(date.format().toString());
         this.setState({
-            startDate: date
+            selectedDate: date.format().toString()
         })
     }
 
+    onDateSelect() {
+        this.props.selectDate(this.state.selectedDate);
+    }
+
     render() {
-        console.log("hooked")
+        // console.log(this.state.startDate);
+        // console.log(this.state.startDate._d);
+        // can use moment.js methods to pull data
         return (
-            <DatePicker selected={ this.state.startDate } onChange={ this.handleChange }></DatePicker>
+            <div>
+                <DatePicker selected={this.state.startDate} onChange={this.handleChange}></DatePicker>
+                <button onClick={this.onDateSelect}>click to throw the date</button>
+            </div>
         )
     }
 }

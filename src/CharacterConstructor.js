dd4e097500cd9ffdcd63cfab63da2782f02c93c6
heymiguel <miguelcesarbautista@gmail.com>
2017-01-29 17:40:11 -0500

    character constructor outputs to console! YOSH!

--- src/CharacterConstructor.js
+++ src/CharacterConstructor.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 // css delcaration goes here
-// import axios from 'axios';
+import axios from 'axios';
 
 class CharacterConstructor extends Component {
     constructor( props ) {
         super( props );
+        this.getCharacterDetails = this.getCharacterDetails.bind( this );
     }
 
     componentDidUpdate() {
-        console.log( this.props );
+        console.log( this.props.characters );
     }
 
 
     getCharacterDetails() {
-        // const myApi = '3bfdbc625fb1b18126abd87d3894d2d4';
-        // const marvelURL = `https://gateway.marvel.com/v1/public/comics?dateRange=${ startDate }%2C${ endDate }`;
-        // axios.get( marvelURL, {
-        //     params: {
-        //         apikey: myApi,
-        //     },
-        // })
+        const myApi = '3bfdbc625fb1b18126abd87d3894d2d4';
+        const marvelURL = 'https://gateway.marvel.com:443/v1/public/characters?';
+        axios.get( marvelURL, {
+            params: {
+                name: this.props.characters[ 1 ],
+                apikey: myApi,
+            },
+        })
+            .then(( res ) => {
+                console.log( res );
+            })
+            .catch(( error ) => {
+                console.log( error );
+            })
     }
 
     render() {
         return (
             <div>
-
+                <button onClick={this.getCharacterDetails}>get character details</button>
             </div>
         );
     }

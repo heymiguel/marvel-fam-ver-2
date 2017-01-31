cfb9cde1d7a2959bc13436c969f174e5fff5e769
heymiguel <miguelcesarbautista@gmail.com>
2017-01-30 22:32:53 -0500

    we are returning arrays properly

--- src/CharacterConstructor.js
+++ src/CharacterConstructor.js
@@ -6,6 +6,7 @@ class CharacterConstructor extends Component {
     constructor( props ) {
         super( props );
         this.getCharacterDetails = this.getCharacterDetails.bind( this );
+        this.completeCharacter = [];
     }
 
     componentDidUpdate() {
@@ -16,18 +17,30 @@ class CharacterConstructor extends Component {
     getCharacterDetails() {
         const myApi = '3bfdbc625fb1b18126abd87d3894d2d4';
         const marvelURL = 'https://gateway.marvel.com:443/v1/public/characters?';
-        axios.get( marvelURL, {
-            params: {
-                name: this.props.characters[ 1 ],
-                apikey: myApi,
-            },
-        })
-            .then(( res ) => {
-                console.log( res );
+        for ( let characterName of this.props.characters ) {
+            axios.get( marvelURL, {
+                params: {
+                    name: characterName,
+                    apikey: myApi,
+                },
             })
-            .catch(( error ) => {
-                console.log( error );
-            })
+                .then(( res ) => {
+                    let character = {
+                        name: characterName,
+                        description: res.data.data.results[ 0 ].description,
+                        image: ""
+                    };
+                    this.completeCharacter.push( character );
+                })
+                .catch(( error ) => {
+                    console.log( error );
+                })
+        }
+        console.log( this.completeCharacter );
+    }
+
+    displayCharacters() {
+
     }
 
     render() {
